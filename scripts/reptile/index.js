2747cb6def507a6f64a965defe412daf25aa0725
guoping <wb-zgp362471@antfin.com>
2018-11-14 17:57:56 +0800

    添加执行步骤和爬取源参数

--- scripts/reptile/index.js
+++ scripts/reptile/index.js
@@ -1,9 +1,9 @@
-const { fork, execSync } = require("child_process");
+const { fork } = require("child_process");
 const { resolve } = require("path");
 const { writeFileSync, openSync, readFileSync, existsSync } = require("fs");
 const moment = require("moment");
 
-execSync(`rm -rf ${__dirname}/**/*.txt`);
+
 
 // module name
 const qukankan = "qukankan";
@@ -12,10 +12,10 @@ const maopu = "maopu";
 const quanben = "quanben";
 
 // module root path
-const qukankanDir = resolve(__dirname, "./www.7kankan.com");
-const aoshiDir = resolve(__dirname, "./www.23zw.me");
-const maopuDir = resolve(__dirname, "./m.maopuzw.com");
-const quanbenDir = resolve(__dirname, "./www.qb520.org");
+const qukankanDir = resolve(__dirname, "./www.7kankan.com"); // a
+const aoshiDir = resolve(__dirname, "./www.23zw.me");        // b
+const maopuDir = resolve(__dirname, "./m.maopuzw.com");      // c
+const quanbenDir = resolve(__dirname, "./www.qb520.org");    // d
 
 const outLogPath = resolve(__dirname, "child-out.log");
 const errorLogPath = resolve(__dirname, "child-error.log");
@@ -28,30 +28,47 @@ const errorLog = openSync(errorLogPath, "w");
 
 const modules = {
     [qukankan]: {
+        sourceAlias: "a",
         dir: qukankanDir,
         list: resolve(qukankanDir, "list.js"),
         detail: require(resolve(qukankanDir, "detail.js")),
         content: resolve(qukankanDir, "content.js"),
     },
     [aoshi]: {
+        sourceAlias: "b",
         dir: aoshiDir,
         list: resolve(aoshiDir, "list.js"),
         detail: require(resolve(aoshiDir, "detail.js")),
         content: resolve(aoshiDir, "content.js"),
     },
     [maopu]: {
+        sourceAlias: "c",
         dir: maopuDir,
         list: resolve(maopuDir, "list.js"),
         detail: require(resolve(maopuDir, "detail.js")),
         content: resolve(maopuDir, "content.js"),
     },
     [quanben]: {
+        sourceAlias: "d",
         dir: quanbenDir,
         list: resolve(quanbenDir, "list.js"),
         detail: require(resolve(quanbenDir, "detail.js")),
         content: resolve(quanbenDir, "content.js"),
     },
 }
+
+const defaultStep = "step=1,2,3";
+const defaultSources = "sources=a,b,c,d";Object.values(modules).reduce((a, {sourceAlias}) => a ? `${a},${sourceAlias}` : sourceAlias ,"")
+
+const args = process.argv.slice(2);
+
+const stepStr = args.find((item) => item.includes("step=")) || defaultStep;
+const steps = stepStr.replace(/step=/g, "").split(/(,|，)/).map((item="") => ~~item.trim());
+
+const sourcesStr = args.find((item) => item.includes("sources=")) || defaultSources;
+const sources = sourcesStr.replace(/sources=/g, "").split(/(,|，)/).map((item="") => item.trim());
+
+
 
 function forkChild(m, cb = () => { }) {
     return new Promise((res) => {
@@ -131,8 +148,13 @@ async function mergeList() {
 
 async function reptileList() {
     return Promise.all(
-        Object.values(modules).map(({ list }) => {
-            return forkChild(list);
+        Object.values(modules).map(({ list, sourceAlias }) => {
+            if (sources.includes(sourceAlias)) {
+                return forkChild(list);
+            } else {
+                console.log("忽略执行：", list);
+                return true;
+            }
         })
     )
 }
@@ -142,11 +164,29 @@ async function start() {
     const start = Date.now();
     console.log(`==========开始时间: ${moment().format("YYYY-MM-DD HH:mm:ss")}==========`);
     console.log("==========开始爬取列表==========");
-    await reptileList();
+    if (steps.includes(1)) {
+        await reptileList();
+    } else {
+        console.log("==========忽略爬取列表==========");
+    }
     console.log(`==========列表爬取结束，开始合并列表 ${moment().format("YYYY-MM-DD HH:mm:ss")}==========`);
-    const list = await mergeList()
+    let list = [];
+    if (steps.includes(2)) {
+        list = await mergeList();
+    } else {
+        try {
+            list = JSON.parse(readFileSync(resolve(__dirname, "./list.json")));
+        } catch (error){
+            console.error("读取文件列表失败", error);
+        }
+    }
     console.log(`==========合并列表完成，开始完善列表信息 ${moment().format("YYYY-MM-DD HH:mm:ss")}==========`);
-    await forkChild(resolve(__dirname, "perfect.js"), (child) => child.send({ list, modules }));
+    if (steps.includes(3)) {
+        await forkChild(resolve(__dirname, "perfect.js"), (child) => child.send({ list, modules }));
+    } else {
+        console.log("==========忽略完善列表==========");
+    }
+    
     console.log("==========完善列表信息结束==========");
     console.log(`==========用时: ${Date.now() - start}ms==========`);
     console.log(`==========结束时间: ${moment().format("YYYY-MM-DD HH:mm:ss")}==========`);
